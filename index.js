7fb3c95ac193f2fa85d0f599c1f59388414c6f76
Brahim HM <brahimhm98@gmail.com>
2023-08-06 11:25:07 +0100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+let employee={person1:{name:'brahim',streetAddress:'alger'}
+,person2:{name:'mohammed',streetAddress:'rouiba'},person3:{name:'Manel',streetAddress:'setif'}};
+
+const updateEmployeeWithKeyAndValue=(employee,key,value)=>{
+   return {...employee,[key]: value};
+};
+
+
+const destructivelyUpdateEmployeeWithKeyAndValue=(employee,key,value)=>{
+    employee[key] = value;
+    return employee;
+ };
+
+const deleteFromEmployeeByKey=(employee,key)=>{
+    let result= {...employee}
+    result[key]=undefined
+    return result
+ };
+
+const destructivelyDeleteFromEmployeeByKey=(employee,key)=>{
+    employee[key] = undefined;
+    return employee;
+ };
+//  console.log(destructivelyUpdateEmployeeWithKeyAndValue(employee,'game','football'))
